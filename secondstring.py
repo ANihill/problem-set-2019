1ebb2f7cf4450060ef8feebc2a1da36ef4f58499
Adam Nihill <adamnihill@gmail.com>
2019-03-20 18:32:26 +0000

    edited comments on problem six to improve legibility

--- secondstring.py
+++ secondstring.py
@@ -1,7 +1,8 @@
 # Adam Nihill, 2019-03-20
 # Program that takes a user input string and outputs every second word
 
-# Set string1 equal to the user input
+# Created a variable called 'String1"
+# String1 contains user input 
 string1 = (input ("Please enter a sentence: "))
 
 # Used slice notation to create a list omitting every second word 
