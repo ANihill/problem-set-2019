462ed154ba7653791c0ec0323aa7f4c3d169898f
Adam Nihill <adamnihill@gmail.com>
2019-03-20 18:42:57 +0000

    added comments to problem one

--- sumupto.py
+++ sumupto.py
@@ -1,14 +1,15 @@
 # Adam Nihill, 2019-03-17
 # Input a positive integer and output the sum of all numbers between one and that number
 
-#
+# Created a variable "sum" containing the integer 0
 sum = 0
+# Variable "I" contains the integer
 i=1
 
-# Set n to user's input
+# Created a variable "n" which contains user's input
 n = int(input("Please enter a positive integer: "))
 
-# 
+# Adds sum to i, storing result in variable i
 while i <= n:
     sum = sum + i
     i+=1
