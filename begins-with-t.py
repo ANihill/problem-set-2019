762e58580b34dd75901933e33d936e35f427a287
Adam Nihill <adamnihill@gmail.com>
2019-03-25 22:25:29 +0000

    removed problem 8 due to an error affecting that also affected the problem 2 python project.

--- begins-with-t.py
+++ begins-with-t.py
@@ -3,11 +3,12 @@
 
 # Call current date
 import datetime
+today = datetime.datetime.today().weekday()
 
 # If current day is Tuesday or Thursday today begins with a "T" 
-if datetime.datetime.today().weekday() == 1:
+if today == 1:
   print("Yes - today begins with a T.")
-elif datetime.datetime.today().weekday() == 3:
+elif today == 3:
   print("Yes - today begins with a T.")
 else:
   print("No - today does not begin with a T.")
--- collatz.py
+++ collatz.py
@@ -1,9 +1,18 @@
+# Adam Nihill, 2019-03-22
+# Collatz Conjecture 
+
+# Declared variable n equal to user's input 
 n = int(input("Please enter a positive integer:"))
 print(n)    
+
+# The while loop will continue until it returns an answer of one 
 while n != 1:
-    if (n % 2 ==0):
+# If n can be divided to two without a remainder it is an even number
+# If it is an even number it will be divided by two 
+    if (n % 2 == 0):
         n = n//2
         print(n)
+# If it is an odd number it will be multiplied by three and one added to result         
     else: 
         n = (3*n) + 1
         print(n)
